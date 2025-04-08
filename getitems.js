c5ed8c39aca938a6dbe3d689877fadc4241c5ba9
jjcrse <jjcrse452@gmmail.com>
2025-04-07 21:08:49 -0500

    se le agrego el .git

--- getitems.js
+++
@@ -1,8 +0,0 @@
-fetch('https://api.artic.edu/api/v1/artworks')
-  .then(response => response.json()) 
-  .then(data => {
-    console.log(data); 
-  })
-  .catch(error => {
-    console.error('Error al obtener los datos:', error);
-  });
